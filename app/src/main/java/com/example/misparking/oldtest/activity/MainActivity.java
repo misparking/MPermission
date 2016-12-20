4c07c627d98dc4a1352bff1147d7080fea7e7a07
misparking <daixingtao@snaillove.com>
2016-12-21 02:17:49 +0800

    首次提交

---
+++ app/src/main/java/com/example/misparking/oldtest/activity/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.misparking.oldtest.activity;
+
+import android.Manifest;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.misparking.oldtest.utils.Constants;
+import com.example.misparking.oldtest.R;
+
+public class MainActivity extends MPermissionsActivity {
+
+    private TextView mTextViewOne, mTextViewTwo;
+    private final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+        initTestFontSize();
+
+
+    }
+
+    private void init() {
+        mTextViewOne = (TextView) this.findViewById(R.id.textView_one);
+        mTextViewTwo = (TextView) this.findViewById(R.id.textView_two);
+
+        Log.e(TAG,getPackageName());
+
+    }
+    /**
+     * 打电话
+     *
+     * @param view
+     */
+    public void onClick_phone(View view) {
+        requestPermission(new String[]{Manifest.permission.CALL_PHONE}, Constants.PERMISSION_CALL_PHONE);
+    }
+
+    /**
+     * 写SD卡
+     *
+     * @param view
+     */
+    public void onClick_sd(View view) {
+        requestPermission(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, Constants.PERMISSION_EXTERNAL_STORAGE);
+    }
+
+    /**
+     * 拍照
+     *
+     * @param view
+     */
+    public void onClick_camera(View view) {
+        requestPermission(new String[]{Manifest.permission.CAMERA}, Constants.PERMISSION_CAMERA);
+    }
+    /**
+     * 定位
+     *
+     * @param view
+     */
+    public void onClick_location(View view) {
+        requestPermission(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, Constants.PERMISSION_LOCATION);
+    }
+
+
+
+
+
+    private void initTestFontSize() {
+        Resources res = getResources();
+        float fontSize = res.getDimension(R.dimen.font_size);
+
+        Log.e("size", "-->" + fontSize);
+        Log.e("size", "-->" + fontSize);
+    }
+}
---
+++ app/src/main/java/com/example/misparking/oldtest/utils/Constants.java
@@ -0,0 +1,20 @@
+package com.example.misparking.oldtest.utils;
+
+public class Constants {
+    /**
+     * 打电话权限
+     */
+    public static final int PERMISSION_CALL_PHONE=0x01;
+    /**
+     * 读写外部存储卡权限
+     */
+    public static final int PERMISSION_EXTERNAL_STORAGE=0x02;
+    /**
+     * 照相权限
+     */
+    public static final int PERMISSION_CAMERA=0x03;
+    /**
+     * 定位
+     */
+    public static final int PERMISSION_LOCATION=0x04;
+}
